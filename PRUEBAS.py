a820d8a88b79237d70388845b18ba8d1f576a2c0
Paula Geier <geierpaulasofia@gmail.com>
2023-09-07 10:24:45 -0300

    Trabajos Practicos

--- PRUEBAS.py
+++
@@ -1,7 +0,0 @@
-objective=int(input('Ingresar el dinero que desea ahorrar: '))
-receive=0
-while receive<objective:
-    money=int(input('Dinero ingresado:'))
-    if money>0:
-        receive+=money
-print(f'Felicitaciones, alcanzo su objetivo. Su dinero ahorrado es de {receive}')
